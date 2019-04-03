f9bf722d77495919e778644b2193df27233da217
theboylim <739034104@qq.com>
2019-04-03 16:46:27 +0800

    测试

--- src/main/java/com/lim/SpringBootDemo/DemoApplication.java
+++ src/main/java/com/lim/SpringBootDemo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("kaishi");
+		System.out.println("kaishiss");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
